4fad7e37f6de1b6780b185ceb9281e6abb6f7893
Mathan <mathanjofficial@gmail.com>
2022-05-31 15:49:53 +0530

    log level changed to warning

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,7 +10,7 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.logger.setLevel(logging.INFO)
+app.logger.setLevel(logging.WARNING)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
 app.logger.addHandler(streamHandler)
