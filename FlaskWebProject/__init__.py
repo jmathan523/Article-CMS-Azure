92b3f025a93812f69bbf243831de2bf67aa9ee44
Mathan <mathanjofficial@gmail.com>
2022-05-31 16:48:13 +0530

    app_logger level changed

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,17 +7,15 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
-from flask.logging import create_logger
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-app_log = create_logger(app)
-app_log.setLevel(logging.INFO)
+app.logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
 streamHandler.setLevel(logging.INFO)
-app_log.addHandler(streamHandler)
+app.logger.addHandler(streamHandler)
 
 Session(app)
 db = SQLAlchemy(app)
